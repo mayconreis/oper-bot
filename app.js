e0114b3eaca3b44429a6da576f790d6f7b673ca8
Maycon Reis <=>
2022-09-05 08:30:02 -0300

    Ajustando horário

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ if (process.env.PORT) {
     channelId = process.env.CHANNEL_ID_TEST
 }
 
-cron.schedule('30 8 * * 1-5', async () => {
-    console.log('App rodando todos os dias as 08:30');
+cron.schedule('35 8 * * 1-5', async () => {
+    console.log('App rodando todos os dias as 08:35');
     mediadorHoje = escolheMediador()
     const data = new Date()
     const dataFormatada = `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
