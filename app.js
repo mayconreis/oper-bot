201020257af617577d5e09864790fc934bb810ca
Maycon Reis <=>
2022-09-06 09:59:51 -0300

    Adicionando rota get para o /

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ require('dotenv').config();
 const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
+    customRoutes: [
+        {
+            path: '/',
+            method: ['GET'],
+            handler: (req, res) => {
+                res.writeHead(200)
+                res.end('App rodando!')
+            }
+        }
+    ]
 });
 
 let sorteados = [];
@@ -31,59 +41,56 @@ if (process.env.PORT) {
     channelId = process.env.CHANNEL_ID_TEST
 }
 
-cron.schedule('35 8 * * 1-5', async () => {
-    console.log('App rodando todos os dias as 08:35');
-    mediadorHoje = escolheMediador()
-    const data = new Date()
-    const dataFormatada = `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
-    console.log(`${dataFormatada} - Mediador de hoje: ${mediadorHoje}`)
-    // Call the chat.postMessage method using the WebClient
-    const result = await app.client.chat.postMessage({
-        channel: channelId,
-        text: "Daily",
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Bora pra Daily! 😍",
-                    "emoji": true
+mediadorHoje = escolheMediador()
+const data = new Date()
+const dataFormatada = `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
+console.log(`${dataFormatada} - Mediador de hoje: ${mediadorHoje}`)
+// Call the chat.postMessage method using the WebClient
+const result = await app.client.chat.postMessage({
+    channel: channelId,
+    text: "Daily",
+    "blocks": [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Bora pra Daily! 😍",
+                "emoji": true
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": `*Mediador: *<@${mediadorHoje}>`
+            }
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Entrar na Daily",
+                        "emoji": true
+                    },
+                    "style": "primary",
+                    "url": "https://meet.google.com/unx-wwvt-mah"
                 }
-            },
-            {
-                "type": "divider"
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `*Mediador: *<@${mediadorHoje}>`
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Entrar na Daily",
-                            "emoji": true
-                        },
-                        "style": "primary",
-                        "url": "https://meet.google.com/unx-wwvt-mah"
-                    }
-                ]
-            },
-            {
-                "type": "divider"
-            },
-        ]
-    }, (err, res) => {
-        if (err) return console.log(err);
-        console.log(res);
-    });
-}, { schedule: true, timezone: 'America/Sao_Paulo' })
+            ]
+        },
+        {
+            "type": "divider"
+        },
+    ]
+}, (err, res) => {
+    if (err) return console.log(err);
+    console.log(res);
+});
 
 // Start your app
 app.start(process.env.PORT || 3000);
